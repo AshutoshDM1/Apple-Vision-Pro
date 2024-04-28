0f9e7d2151644266648debe52500703f972b3bf0
AshutoshDM1 <downlodemaster1@gmail.com>
2024-04-28 15:45:06 +0530

    complete page3

--- index.js
+++ index.js
@@ -31,14 +31,43 @@ let tl = gsap.timeline({});
 tl.to("#page2>h1",{
   y:-1000,
   duration:4,
-  delay:5,
+  // delay:5,
   scrollTrigger: {
     trigger: "#page2",
     scroller: "#main" ,
-    markers: true,
+    // markers: true,
     start : "top 0%",
     end : "top -20%",
     scrub: 4,
     pin:true,
+  },
+})
+tl.to("#page3>h1",{
+  y:-1000,
+  duration:4,
+  // delay:5,
+  scrollTrigger: {
+    trigger: "#page3",
+    scroller: "#main" ,
+    // markers: true,
+    start : "top 0%",
+    end : "top -20%",
+    scrub: 4,
+    pin:true,
+  },
+})
+let scrollbar = document.querySelector('.c-scrollbar_thumb ');
+gsap.to("#page4",{
+  onStart:function() {
+    console.log("page 4");
+    document.querySelector("#page4").addEventListener("mouseenter", function(e) {
+      scrollbar.style.backgroundColor = "Black"
+    })
+    document.querySelector("#page4").addEventListener("mouseover", function(e) {
+      scrollbar.style.backgroundColor = "Black"
+    })
+    document.querySelector("#page4").addEventListener("mouseleave", function(e) {
+      scrollbar.style.backgroundColor = "white"
+    })
   },
 })
